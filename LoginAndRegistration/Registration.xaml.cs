01552bc3235b85488631f432fb452d9587a44c1c
yunov.demid@mail.ru <yunov.demid@mail.ru>
2021-03-08 18:34:15 +0300

    Проверка

---
+++ LoginAndRegistration/Registration.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CourtExpert
+{
+    public partial class Registration : Window
+    {
+        string connectionString;
+        public Registration()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["CourtExpertConnectionString"].ConnectionString;
+        }
+
+        private void ButtonLog_Click(object sender, RoutedEventArgs e)
+        {
+            bool english = true;
+            bool number = false;
+            string message = ""; 
+
+            if (PasswordBox.Password == PasswordBoxRepeat.Password)
+            {
+                for (int i = 0; i < PasswordBox.Password.Length; i++) // перебираем символы
+                {
+                    if (PasswordBox.Password[i] >= 'А' && PasswordBox.Password[i] <= 'Я')
+                        english = false; 
+                    if (PasswordBox.Password[i] >= '0' && PasswordBox.Password[i] <= '9') 
+                        number = true; 
+                }
+
+                if (!english)
+                {
+                    message += "Доступна только английская раскладка\n"; 
+                } 
+                 
+                if (!number)
+                {
+                    message += "Добавьте хотя бы одну цифру\n"; 
+                }
+                
+                if (english && number) 
+                {
+                    string sqlSave = "insert into Employee values (@LoginEmployee, @PasswordEmployee, 2)";
+                    DataBaseRegistration(sqlSave);
+                    this.Close();
+                }
+                     
+                if (PasswordBox.Password.Length < 6)
+                {
+                    message += "Пароль слишком короткий, минимум 6 символов\n"; 
+                }
+
+                LabelMeessage.Content = message;
+            }
+        }
+        private void DataBaseRegistration(string sql)
+        {
+            DataTable employeeDT = new DataTable();
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.Add("@LoginEmployee", SqlDbType.VarChar).Value = TextBoxLogin.Text;
+            command.Parameters.Add("@PasswordEmployee", SqlDbType.VarChar).Value = PasswordBox.Password;
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
---
+++ UsersWindow/EmployeeWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using CourtExpert.FormsToFillOut;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CourtExpert.UsersWindow
+{
+    public partial class EmployeeWindow : Window
+    {
+        string connectionString;
+        public EmployeeWindow()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["CourtExpertConnectionString"].ConnectionString;
+        }
+
+        private void ButtonClientCardClick(object sender, RoutedEventArgs e)
+        {
+            PersoneClientWindow form1 = new PersoneClientWindow();
+            form1.Show();
+        }
+
+        private void ButtonLegalEntityClick(object sender, RoutedEventArgs e)
+        {
+            LegalEntityWindow form2 = new LegalEntityWindow();
+            form2.Show();
+        }
+
+        private void ButtonIndividualPersonClick(object sender, RoutedEventArgs e)
+        {
+            PersoneClientWindow personeClientWindow = new PersoneClientWindow();
+            personeClientWindow.Show();
+        }
+        private void DataBasePersoneClient(string sql)
+        {
+            DataTable employeeDT = new DataTable();
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.Add("@LoginEmployee", SqlDbType.VarChar).Value = TextBoxLogin.Text;
+            command.Parameters.Add("@PasswordEmployee", SqlDbType.VarChar).Value = PasswordBox.Password;
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+    public class DataClient
+    {
+        public string FamilyName = "";
+        public string FirstName = "";
+        public int Patronimic = 0;
+        public int Sex = 0;
+        public int BirthDate = 0;
+        public int BirthPlace = 0;
+        public int PassportIssued = 0;
+        public int DatePasportIssue = 0;
+        public int DepartamentCode = 0;
+        public int PasportSeries = 0;
+        public int PassportNumber = 0;
+        public int NumberINN = 0;
+        public int NumberSnils = 0;
+    }
+}
